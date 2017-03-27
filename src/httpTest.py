a621c5c26c4a049a8947dc3a13cc3f9e6b211873
root <254387211@qq.com>
2017-03-27 08:12:48 -0700

    http_test

---
+++ src/httpTest.py
@@ -0,0 +1,24 @@
+import threading
+from urllib import request
+
+url = 'http://act.zb.mi.com/anniversay-user-account/activity/queryInfo?uuid='
+
+
+def query_info(uuid):
+    url_path = url + str(uuid)
+    with request.urlopen(url_path) as f:
+        # print('status:', f.status, f.reason)
+        # for k, v in f.getheaders():
+        #     print('%s: %s' % (k, v))
+        print('Data:', f.read().decode('utf-8'))
+
+
+count, uuid = 1, 21904369
+while count < 20:
+    t = threading.Thread(target=query_info(uuid), name="query_info" + str(count))
+    t.start()
+    t.join()
+    count += 1
+    uuid += count
+
+print('thread %s ended.' % threading.current_thread().name)
